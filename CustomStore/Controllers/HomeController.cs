87822be687a258a80432d36c34af9fe817bf3bce
lostaunaum <lostaunaum@gmail.com>
2015-05-11 18:23:05 -0500

    Adding home page controller

--- CustomStore/Controllers/HomeController.cs
+++ CustomStore/Controllers/HomeController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Http;
 
 namespace CustomStore.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : ApiController
     {
+        //
+        // GET: /Home/
+
         public ActionResult Index()
         {
             return View();
         }
+
     }
 }
